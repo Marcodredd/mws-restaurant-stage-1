5f05b04edc86f234b35d0f08b38f19eac99f3150
Lete Fabani Nwibasi <lete.nwibasi@gmail.com>
2018-08-22 15:38:18 +0100

    Base URL for github page

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,8 +8,14 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8080 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+   /* const port = 8080 // Change this to your server port
+    return `http://localhost:${port}/data/restaurants.json`; */
+    const getBaseUrl = () => {
+      let baseUrl = window.location.origin;
+      if (window.location.pathname.includes(config.'/mws-restaurant-stage-1'))
+        baseUrl += '/mws-restaurant-stage-1';
+      return baseUrl;
+    }
   }
 
   /**
